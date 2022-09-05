afd087295ed2782631e91fc89e20549cda206863
Nebula <me@itsnebula.net>
2022-09-05 09:42:39 -0400

    merge upstream changes

--- Helper/main.m
+++ Helper/main.m
@@ -1,7 +1,7 @@
 @import Foundation;
 
-BOOL delete(NSString* path) {
-    return [[NSFileManager defaultManager] removeItemAtPath:path error:nil];
+BOOL delete(NSString* path, NSError** error) {
+    return [[NSFileManager defaultManager] removeItemAtPath:path error:error];
 }
 
 int main(int argc, char *argv[], char *envp[]) {
@@ -11,6 +11,7 @@ int main(int argc, char *argv[], char *envp[]) {
         NSLog(@"[SantanderHelper] spawned, uid: %d, gid: %d", getuid(), getgid());
 
         int ret = 0;
+        NSError* error;
 
         NSString* cmd = [NSString stringWithUTF8String:argv[1]];
         if([cmd isEqualToString:@"delete"]) {
@@ -18,7 +19,11 @@ int main(int argc, char *argv[], char *envp[]) {
             NSString* path = [NSString stringWithUTF8String:argv[2]];
             NSLog(@"[SantanderHelper] called delete: %@", path);
 
-            delete(path);
+            delete(path, &error);
+        }
+
+        if (error) {
+            NSLog(@"[SantanderHelper] error: %@", [error localizedDescription]);
         }
 
         NSLog(@"[SantanderHelper] returning %d", ret);
--- build.sh
+++ build.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
 
+ORIGDIR=`pwd`
+BUILDDIR=`pwd`/xcodebuild
+
+echo "[0] Clean"
+rm -rf out xcodebuild Helper/.theos
+
 echo "[1] Build app"
 cd App
-xcodebuild build -scheme Santander -configuration "Release" CODE_SIGNING_ALLOWED="NO" CODE_SIGNING_REQUIRED="NO" CODE_SIGN_IDENTITY="" BUILD_DIR=../xcodebuild
-cd ..
+xcodebuild build -scheme Santander -configuration "Release" CODE_SIGNING_ALLOWED="NO" CODE_SIGNING_REQUIRED="NO" CODE_SIGN_IDENTITY="" BUILD_DIR=$BUILDDIR
 
 echo "[2] Build helper"
-cd Helper
+cd ../Helper
 make clean
 make FINALPACKAGE=1
-
-cp .theos/obj/santanderhelper ../out/ipa/Payload/Santander.app/santanderhelper
-ldid -s -K../cert.p12 -Upassword ../out/ipa/Payload/Santander.app/santanderhelper
+cd $ORIGDIR
 
 echo "[3] Package IPA"
 mkdir -p out/ipa/Payload
 cp -R xcodebuild/Release-iphoneos/Santander.app out/ipa/Payload
+cp Helper/.theos/obj/santanderhelper out/ipa/Payload/Santander.app/santanderhelper
+
 ldid -SApp/entitlements.plist out/ipa/Payload/Santander.app
+
 cd out/ipa
 zip -r ../Santander.ipa .
-cd ../..
-rm -rf out/ipa xcodebuild
+cd $ORIGDIR
+rm -rf xcodebuild
 
 echo "IPA is in out/Santander.ipa"
