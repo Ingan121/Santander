d9be930ea0288543bb12934be946b354056d514c
Nebula <me@itsnebula.net>
2022-09-07 15:43:40 -0400

    file structure changes

--- build.sh
+++ build.sh
@@ -7,11 +7,10 @@ echo "[0] Clean"
 rm -rf out xcodebuild Helper/.theos
 
 echo "[1] Build app"
-cd App
 xcodebuild build -scheme Santander -configuration "Release" CODE_SIGNING_ALLOWED="NO" CODE_SIGNING_REQUIRED="NO" CODE_SIGN_IDENTITY="" BUILD_DIR=$BUILDDIR
 
 echo "[2] Build helper"
-cd ../Helper
+cd Helper
 make clean
 make FINALPACKAGE=1
 cd $ORIGDIR
@@ -21,7 +20,7 @@ mkdir -p out/ipa/Payload
 cp -R xcodebuild/Release-iphoneos/Santander.app out/ipa/Payload
 cp Helper/.theos/obj/santanderhelper out/ipa/Payload/Santander.app/santanderhelper
 
-ldid -SApp/entitlements.plist out/ipa/Payload/Santander.app
+ldid -Sentitlements.plist out/ipa/Payload/Santander.app
 
 cd out/ipa
 zip -r ../Santander.ipa .
